2f94543a6d1e309716d1df276794b10443d68fd7
KimHS0915 <remember2552@gmail.com>
2022-05-14 13:28:26 +0900

    Add useFadeIn, useNetwork

---
+++ react/hooks/useNetwork.js
@@ -0,0 +1,18 @@
+export const useNetwork = (onChange) => {
+  const [status, setStatus] = useState(navigator.onLine);
+  const handleChange = useCallback(() => {
+    if (typeof onChange === "function") {
+      onChange(navigator.onLine);
+    }
+    setStatus(navigator.onLine);
+  }, [onChange]);
+  useEffect(() => {
+    window.addEventListener("online", handleChange);
+    window.addEventListener("offline", handleChange);
+    return () => {
+      window.removeEventListener("online", handleChange);
+      window.removeEventListener("offline", handleChange);
+    };
+  }, [handleChange]);
+  return status;
+};
