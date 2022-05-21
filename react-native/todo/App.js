10aca483d7d57e88ce6416f6f6e72faba9b3add5
KimHS0915 <remember2552@gmail.com>
2022-05-21 23:08:14 +0900

    Add header

--- react-native/todo/App.js
+++ react-native/todo/App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { theme } from "./colors";
 
 export default function App() {
+  const [home, setHome] = useState(true);
+  const toHome = () => setHome(true);
+  const toOffice = () => setHome(false);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={toHome}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: home ? theme.activation : theme.deactivation,
+            }}
+          >
+            Home
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={toOffice}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !home ? theme.activation : theme.deactivation,
+            }}
+          >
+            Office
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +39,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    marginTop: 100,
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+  btnText: {
+    fontSize: 40,
+    fontWeight: "500",
   },
 });
---
+++ react-native/todo/colors.js
@@ -0,0 +1,5 @@
+export const theme = {
+  bg: "black",
+  activation: "#FFFFFF",
+  deactivation: "#505050",
+};
