b553c64f6a480bec0cdd3fe323246701f2648d54
KimHS0915 <remember2552@gmail.com>
2022-05-31 20:04:47 +0900

    Refactoring with redux toolkit

--- redux/react-redux/src/store.js
+++ redux/react-redux/src/store.js
@@ -1,29 +1,27 @@
-import { combineReducers, createStore } from "redux";
-import { persistStore } from "redux-persist";
-import persistReducer from "redux-persist/es/persistReducer";
+import { combineReducers } from "redux";
+import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
-const addToDo = (text) => {
-  return { type: ADD, text };
-};
-
-const deleteToDo = (id) => {
-  return { type: DELETE, id };
-};
-
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
-    default:
-      return state;
-  }
-};
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) =>
+    state.filter((toDo) => toDo.id !== action.payload),
+});
 
 const persistConfig = {
   key: "toDo",
@@ -31,8 +29,17 @@ const persistConfig = {
 };
 
 const allReducer = combineReducers({ reducer });
-
-export const store = createStore(persistReducer(persistConfig, allReducer));
+const persistedReducer = persistReducer(persistConfig, allReducer);
+// export const store = createStore(persistReducer(persistConfig, allReducer));
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
 export const actionCreators = {
   addToDo,
